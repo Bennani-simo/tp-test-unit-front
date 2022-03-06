7c1923d57452eecd22a5a9364e56ce4f41e2ce34
bennani <simobenn96@gmail.com>
2022-03-06 22:50:14 +0100

    CYPRESS

---
+++ cypress/integration/mock.spec.ts
@@ -0,0 +1,17 @@
+describe('Example to demonstrate API Mocking in Cypress', () => {
+
+  beforeEach(() => {
+      cy.server()
+      cy.route('GET', '**/products', 'fixture:rickmorty.json')
+
+      cy.visit('http://localhost:4200/')
+  })
+
+  it('Mock the Tags from the API Response and then validate on UI', () => {
+    cy.get(".flex-item")
+    .find("li")
+    .should("have.length", 20);
+
+  })
+
+})
---
+++ cypress/integration/product-page.spec.ts
@@ -0,0 +1,68 @@
+describe("header selector", () => {
+  beforeEach(() => {
+    cy.intercept('/rickmorty', { fixture: 'rickmorty.json' });
+    cy.visit("http://localhost:4200");
+  });
+
+  it("Application header contain text", () => {
+    cy.get(".app")
+      .find("span")
+      .should("contain.text", "Application de ouuuf");
+  });
+
+
+  it("Available product contains 20 item", () => {
+    cy.get(".flex-item")
+      .find("li")
+      .should("have.length", 20);
+ });
+
+
+ it("ajout au panier", () => {
+  cy.get(".detail").first()
+  .find("button")
+  .click()
+});
+
+
+
+ it('redirect to panier', () => {
+    cy.get(".button1")
+    .find("button")
+    .click()
+    cy.location('pathname').should('eq', '/cart')
+});
+
+
+it("La page panier est affiché", () => {
+  cy.get(".app")
+    .find("button:first")
+    .click()
+    cy.get(".test2")
+    .find("h1")
+    .should("contain.text", "Mon Panier");
+});
+
+
+it("IL Y'a un article dans le panier", () => {
+  cy.get(".app")
+    .find("button:first")
+    .click()
+    cy.get(".test2")
+    .find("li")
+    .should("have.length", 1);
+});
+
+
+it('redirect to STORE', () => {
+  cy.get(".button2")
+//cy.get('input[name=description]').type('Hello World')
+  .find("button")
+  .click()
+
+  cy.location('pathname').should('eq', '/product-list')
+});
+
+});
+
+
